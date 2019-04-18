20d425fead23759f05851e36f952522788ed881a
Brendan Taylor <Brendan.J.Taylor-1@ou.edu>
2019-04-18 13:41:23 -0500

    Background color and show grid lines

--- Connect4Project/GameBoard/MainWindow.xaml.cs
+++ Connect4Project/GameBoard/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace GameBoard
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
